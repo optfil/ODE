da4a1543b56b440258adbf32b9953c47788ab596
optfil <dergachev88@gmail.com>
2018-02-07 14:36:51 +0300

    Sliders added.

--- form.cpp
+++ form.cpp
@@ -15,10 +15,28 @@ Form::Form(QWidget *parent)
     labelSizeT_2 = new QLabel(tr("T = "));
     labelSizeT_2->setAlignment(Qt::AlignRight);
     spinBoxSizeT = new QSpinBox();
+    spinBoxSizeT->setMinimum(1);
+    spinBoxSizeT->setMaximum(20);
     labelNT_1 = new QLabel(tr("Integration points"));
     labelNT_2 = new QLabel(tr("NT = "));
     labelNT_2->setAlignment(Qt::AlignRight);
     spinBoxNT = new QSpinBox();
+    spinBoxNT->setMinimum(1);
+    spinBoxNT->setMaximum(100);
+
+    sliderSizeT = new QSlider(Qt::Horizontal);
+    sliderSizeT->setRange(kTMin, kTMax);
+    sliderSizeT->setSingleStep(1);
+    sliderSizeT->setTickInterval(1);
+    sliderSizeT->setTickPosition(QSlider::TicksBelow);
+    sliderSizeT->setValue(1);
+
+    sliderNT = new QSlider(Qt::Horizontal);
+    sliderNT->setRange(kNtMin, kNtMax);
+    sliderNT->setTickInterval(10);
+    sliderNT->setTickPosition(QSlider::TicksBelow);
+    sliderNT->setValue(kNtMin);
+
     labelDT_1 = new QLabel(tr("Integration step"));
     labelDT_2 = new QLabel(tr("∆t = "));
     labelDT_2->setAlignment(Qt::AlignRight);
@@ -26,20 +44,52 @@ Form::Form(QWidget *parent)
 
     QGridLayout *layoutParam = new QGridLayout();
     layoutParam->addWidget(labelEquation, 0, 0, 1, 1);
-    layoutParam->addWidget(comboBoxEquation, 0, 1, 1, 2);
+    layoutParam->addWidget(comboBoxEquation, 0, 1, 1, 3);
     layoutParam->addWidget(labelSizeT_1, 1, 0, 1, 1, Qt::AlignBaseline);
     layoutParam->addWidget(labelSizeT_2, 1, 1, 1, 1, Qt::AlignBaseline);
     layoutParam->addWidget(spinBoxSizeT, 1, 2, 1, 1);
+    layoutParam->addWidget(sliderSizeT, 1, 3, 1, 1);
     layoutParam->addWidget(labelNT_1, 2, 0, 1, 1, Qt::AlignBaseline);
     layoutParam->addWidget(labelNT_2, 2, 1, 1, 1, Qt::AlignBaseline);
     layoutParam->addWidget(spinBoxNT, 2, 2, 1, 1);
+    layoutParam->addWidget(sliderNT, 2, 3, 1, 1);
     layoutParam->addWidget(labelDT_1, 3, 0, 1, 1);
     layoutParam->addWidget(labelDT_2, 3, 1, 1, 1);
     layoutParam->addWidget(labelDT, 3, 2, 1, 1);
 
     setLayout(layoutParam);
+
+    connect(sliderSizeT, SIGNAL(valueChanged(int)), this, SLOT(update_sizet(int)));
+    connect(sliderNT, SIGNAL(valueChanged(int)), this, SLOT(update_nt(int)));
+    connect(spinBoxSizeT, SIGNAL(valueChanged(int)), this, SLOT(update_sizet(int)));
+    connect(spinBoxNT, SIGNAL(valueChanged(int)), this, SLOT(update_nt(int)));
+
 }
 
 Form::~Form()
 {
+}
+
+void Form::update_sizet(int n)
+{
+    spinBoxSizeT->blockSignals(true);
+    sliderSizeT->blockSignals(true);
+
+    sliderSizeT->setValue(n);
+    spinBoxSizeT->setValue(n);
+
+    spinBoxSizeT->blockSignals(false);
+    sliderSizeT->blockSignals(false);
+}
+
+void Form::update_nt(int n)
+{
+    spinBoxNT->blockSignals(true);
+    sliderNT->blockSignals(true);
+
+    sliderNT->setValue(n);
+    spinBoxNT->setValue(n);
+
+    spinBoxNT->blockSignals(false);
+    sliderNT->blockSignals(false);
 }
--- form.h
+++ form.h
@@ -4,7 +4,14 @@
 #include <QWidget>
 #include <QLabel>
 #include <QComboBox>
+#include <QSlider>
 #include <QSpinBox>
+
+
+constexpr int kTMin = 1;
+constexpr int kTMax = 20;
+constexpr int kNtMin = 2;
+constexpr int kNtMax = 100;
 
 class Form : public QWidget
 {
@@ -17,10 +24,16 @@ public:
     enum EquationType {Decay, Grow, Oscillate};
     Q_ENUM(EquationType)
 
+private slots:
+    void update_nt(int n);
+    void update_sizet(int n);
+
+private:
     QLabel *labelEquation;
     QComboBox *comboBoxEquation;
     QLabel *labelSizeT_1, *labelSizeT_2, *labelNT_1, *labelNT_2;
     QSpinBox *spinBoxSizeT, *spinBoxNT;
+    QSlider *sliderSizeT, *sliderNT;
     QLabel *labelDT_1, *labelDT_2, *labelDT;
 };
 
