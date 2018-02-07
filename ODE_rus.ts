b9ce7b5482a9eb1e50c146798130ddae6f7e6539
optfil <dergachev88@gmail.com>
2018-02-07 15:00:54 +0300

    License added

---
+++ ODE_rus.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="form.cpp" line="8"/>
+        <source>Equation type</source>
+        <translation>Тип уравнения</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="10"/>
+        <source>y&apos; = -y</source>
+        <translation>y&apos; = -y</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="11"/>
+        <source>y&apos; = y</source>
+        <translation>y&apos; = y</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="12"/>
+        <source>y&apos; = iy</source>
+        <translation>y&apos; = iy</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="14"/>
+        <source>Integration time</source>
+        <translation>Время расчета</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="15"/>
+        <source>T = </source>
+        <translation>T = </translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="18"/>
+        <source>Integration points</source>
+        <translation>Количество шагов</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="19"/>
+        <source>NT = </source>
+        <translation>NT = </translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="22"/>
+        <source>Integration step</source>
+        <translation>Шаг интегрирования</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="23"/>
+        <source>∆t = </source>
+        <translation>∆t = </translation>
+    </message>
+</context>
+</TS>
