99d923100a67ccb359faffa1244933e1d8a79391
optfil <dergachev88@gmail.com>
2018-02-08 18:51:05 +0300

    Russian translations for other methods tabs done.

--- ODE_rus.ts
+++ ODE_rus.ts
@@ -4,72 +4,96 @@
 <context>
     <name>Form</name>
     <message>
-        <location filename="form.cpp" line="8"/>
+        <location filename="form.cpp" line="84"/>
         <source>Equation type</source>
         <translation>Тип уравнения</translation>
     </message>
     <message>
-        <location filename="form.cpp" line="10"/>
+        <location filename="form.cpp" line="86"/>
         <source>y&apos; = -y</source>
         <translation>y&apos; = -y</translation>
     </message>
     <message>
-        <location filename="form.cpp" line="11"/>
+        <location filename="form.cpp" line="87"/>
         <source>y&apos; = y</source>
         <translation>y&apos; = y</translation>
     </message>
     <message>
-        <location filename="form.cpp" line="12"/>
+        <location filename="form.cpp" line="88"/>
         <source>y&apos; = iy</source>
         <translation>y&apos; = iy</translation>
     </message>
     <message>
-        <location filename="form.cpp" line="14"/>
+        <location filename="form.cpp" line="90"/>
         <source>Integration time</source>
         <translation>Время расчета</translation>
     </message>
     <message>
-        <location filename="form.cpp" line="15"/>
+        <location filename="form.cpp" line="91"/>
         <source>T = </source>
         <translation>T = </translation>
     </message>
     <message>
-        <location filename="form.cpp" line="20"/>
+        <location filename="form.cpp" line="96"/>
         <source>Integration points</source>
         <translation>Количество шагов</translation>
     </message>
     <message>
-        <location filename="form.cpp" line="21"/>
+        <location filename="form.cpp" line="97"/>
         <source>NT = </source>
         <translation>NT = </translation>
     </message>
     <message>
-        <location filename="form.cpp" line="40"/>
+        <location filename="form.cpp" line="116"/>
         <source>Integration step</source>
         <translation>Шаг интегрирования</translation>
     </message>
     <message>
-        <location filename="form.cpp" line="41"/>
+        <location filename="form.cpp" line="118"/>
         <source>∆t = </source>
         <translation>∆t = </translation>
     </message>
     <message>
-        <location filename="form.cpp" line="51"/>
+        <location filename="form.cpp" line="128"/>
+        <location filename="form.cpp" line="211"/>
+        <location filename="form.cpp" line="294"/>
+        <location filename="form.cpp" line="377"/>
+        <source>Solution</source>
+        <translation>Решение</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="154"/>
+        <location filename="form.cpp" line="237"/>
+        <location filename="form.cpp" line="320"/>
+        <location filename="form.cpp" line="403"/>
+        <source>Local error</source>
+        <translation>Локальная погрешность</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="178"/>
+        <location filename="form.cpp" line="261"/>
+        <location filename="form.cpp" line="344"/>
+        <location filename="form.cpp" line="427"/>
+        <source>Global error</source>
+        <translation>Глобальная погрешность</translation>
+    </message>
+    <message>
+        <location filename="form.cpp" line="456"/>
         <source>Euler</source>
         <translation>Метод Эйлера</translation>
     </message>
     <message>
-        <location filename="form.cpp" line="52"/>
+        <location filename="form.cpp" line="457"/>
         <source>Leap frog</source>
         <translation>Метод с перешагиванием</translation>
     </message>
     <message>
-        <location filename="form.cpp" line="53"/>
+        <location filename="form.cpp" line="458"/>
         <source>Two-step</source>
         <translation>Явный двухшаговый метод</translation>
     </message>
     <message>
-        <location filename="form.cpp" line="54"/>
+        <location filename="form.cpp" line="459"/>
         <source>Runge-Kutta</source>
         <translation>Метод Рунге-Кутты</translation>
     </message>
